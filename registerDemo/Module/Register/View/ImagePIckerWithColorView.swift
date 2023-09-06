10eb58ec95699af4dc2c4a63f76f5a243d1a397e
baimiao <xubaimiao@ourpalm.com>
2023-09-06 18:08:23 +0800

    selectImage from photoLibrary

--- registerDemo/Module/Register/View/ImagePIckerWithColorView.swift
+++ registerDemo/Module/Register/View/ImagePIckerWithColorView.swift
@@ -11,7 +11,7 @@ import PhotosUI
 
 class ImagePIckerWithColorView: UIView, UIColorPickerViewControllerDelegate, PHPickerViewControllerDelegate {
     
-    
+    private var avatarImageViewModel: AvatarImagePIckerViewModel;
     
     private let avatarImageView = UIImageView();
     private let avatarImagePickerButton = UIButton();
@@ -19,12 +19,15 @@ class ImagePIckerWithColorView: UIView, UIColorPickerViewControllerDelegate, PHP
     private var cancellables: Set<AnyCancellable> = [];
     
     init(viewModel: AvatarImagePIckerViewModel) {
-        super.init(frame: .zero)
+        avatarImageViewModel = viewModel;
+        super.init(frame: .zero);
+        
         setupUI()
+//        avatarImageViewModel = viewModel;
 
         viewModel.avatarImage
             .sink { [weak self] image in
-//                self?.avatarImageView.image = image
+                self?.avatarImageView.image = image
             }
             .store(in: &cancellables)
 
@@ -75,39 +78,49 @@ class ImagePIckerWithColorView: UIView, UIColorPickerViewControllerDelegate, PHP
         ]);
     }
     
-    // button Clicked
+    // MARK: button Clicked
     @objc func colorPickerButtonTapped() {
         let colorPicker = UIColorPickerViewController();
         colorPicker.delegate = self;
-        if #available(iOS 15, *) {
-            if let windowScene = UIApplication.shared.connectedScenes
-                .compactMap({ $0 as? UIWindowScene })
-                .first(where: { $0.activationState == .foregroundActive }) {
-
-                if let topViewController = windowScene.windows
-                    .filter({ $0.isKeyWindow })
-                    .first?
-                    .rootViewController {
-                    topViewController.present(colorPicker, animated: true, completion: nil);
-
-                }
-            }
-        } else {
-            if let topViewController = UIApplication.shared.windows.first?.rootViewController {
-                // topViewController 现在是当前的视图控制器
-                print("当前视图控制器：\(topViewController)")
-                topViewController.present(colorPicker, animated: true, completion: nil);
-            }
-        }
+        presentNewVC(viewController: colorPicker);
     }
     
     @objc func imagePickeButtonTaped() {
+        PHPhotoLibrary.requestAuthorization { status in
+               switch status {
+               case .authorized:
+                   // 用户已授权相册访问权限，可以执行相册操作
+                   DispatchQueue.main.async {
+                       self.popUpImagePicker();
+                   }
+                   break
+               case .denied, .restricted:
+                   // 用户拒绝了相册访问权限，或者权限受到了限制
+                   // 提示用户打开设置中的隐私设置以授予权限
+                   break
+               case .notDetermined:
+                   // 用户还没有做出选择，可以在此提供一个说明或者再次请求权限
+                   break
+               @unknown default:
+                   break
+               }
+           }
+        
+        
+    }
+    
+    // MARK: private Method
+    // 唤起相册
+    private func popUpImagePicker() {
         var configuration = PHPickerConfiguration()
         configuration.filter = .images  // 过滤为图片
         configuration.selectionLimit = 1  // 设置选择的图片数量限制
         
         let picker = PHPickerViewController(configuration: configuration)
         picker.delegate = self
+        presentNewVC(viewController: picker);
+    }
+    private func presentNewVC(viewController: UIViewController) {
         if #available(iOS 15, *) {
             if let windowScene = UIApplication.shared.connectedScenes
                 .compactMap({ $0 as? UIWindowScene })
@@ -117,7 +130,7 @@ class ImagePIckerWithColorView: UIView, UIColorPickerViewControllerDelegate, PHP
                     .filter({ $0.isKeyWindow })
                     .first?
                     .rootViewController {
-                    topViewController.present(picker, animated: true, completion: nil);
+                    topViewController.present(viewController, animated: true, completion: nil);
                     
                 }
             }
@@ -125,16 +138,31 @@ class ImagePIckerWithColorView: UIView, UIColorPickerViewControllerDelegate, PHP
             if let topViewController = UIApplication.shared.windows.first?.rootViewController {
                 // topViewController 现在是当前的视图控制器
                 print("当前视图控制器：\(topViewController)")
-                topViewController.present(picker, animated: true, completion: nil);
+                topViewController.present(viewController, animated: true, completion: nil);
             }
         }
-        
     }
     
-    // photo picker delegate method
+    // MARK: photo picker delegate method
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        
+//        dismiss(animated: true, completion: nil)
+        picker.dismiss(animated: true, completion: nil);
+        let itemProvider = results.first?.itemProvider
+        itemProvider?.loadObject(ofClass: UIImage.self) { (loadedObject, error) in
+            if let error = error {
+                // Handle the error, e.g., print an error message or display an alert.
+                print("Error loading image: \(error)")
+            } else if let image = loadedObject as? UIImage {
+                // You have successfully loaded the UIImage, now you can use it.
+                DispatchQueue.main.async {
+                    self.avatarImageViewModel.selectedAvatarImage.send(image);
+                    // Update your UI with the loaded image, e.g., assign it to an imageView.
+//                    self.imageView.image = image
+                }
+            }
+        }
     }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
